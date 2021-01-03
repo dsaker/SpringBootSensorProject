d2f27843d1e0cfe1b231d5dcc669d3c089e37bd9
Dusty Saker <dustysaker@gmail.com>
2021-01-03 06:37:47 -0800

    first commit

---
+++ src/test/java/net/iwillwork4u/demo/SensorsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.iwillwork4u.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

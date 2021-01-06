ca81bba07f07235c171e6b702a77418552108fa3
Dusty Saker <dustysaker@gmail.com>
2021-01-06 07:05:20 -0800

    basic hateoas

--- src/main/java/net/iwillwork4u/sensors/sensor/SensorController.java
+++ src/main/java/net/iwillwork4u/sensors/sensor/SensorController.java
@@ -2,6 +2,8 @@ package net.iwillwork4u.sensors.sensor;
 
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,32 +17,35 @@ public class SensorController {
 
     private final SensorRepository repository;
 
-    SensorController(SensorRepository repository) {
+    private final SensorModelAssembler assembler;
+
+    SensorController(SensorRepository repository, SensorModelAssembler assembler) {
+
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/sensors")
     CollectionModel<EntityModel<Sensor>> all() {
         List<EntityModel<Sensor>> sensors = repository.findAll().stream()
-                .map(sensor -> EntityModel.of(sensor,
-                        linkTo(methodOn(SensorController.class).one(sensor.getSid())).withSelfRel(),
-                        linkTo(methodOn(SensorController.class).all()).withRel("sensors"))).collect(Collectors.toList());
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
         return CollectionModel.of(sensors,
                 linkTo(methodOn(SensorController.class).all()).withSelfRel());
     }
 
     @PostMapping("/sensors")
-    Sensor newSensor(@RequestBody Sensor newSensor) {
-        return repository.save(newSensor);
+    ResponseEntity<EntityModel<Sensor>> newSensor(@RequestBody Sensor newSensor) {
+
+        EntityModel<Sensor> entityModel = assembler.toModel(repository.save(newSensor));
+        return ResponseEntity.created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()).body(entityModel);
     }
 
     @GetMapping("/sensors/{id}")
     EntityModel<Sensor> one(@PathVariable Long id) {
         Sensor sensor = repository.findById(id).orElseThrow(() ->
                 new SensorNotFoundException(id));
-        return EntityModel.of(sensor,
-                linkTo(methodOn(SensorController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(SensorController.class).all()).withRel("sensors"));
+        return assembler.toModel(sensor);
     }
 
 }
---
+++ src/main/java/net/iwillwork4u/sensors/sensor/SensorModelAssembler.java
@@ -0,0 +1,20 @@
+package net.iwillwork4u.sensors.sensor;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Component
+class SensorModelAssembler implements RepresentationModelAssembler<Sensor, EntityModel<Sensor>> {
+
+    @Override
+    public  EntityModel<Sensor> toModel(Sensor sensor) {
+        return EntityModel.of(sensor,
+                linkTo(methodOn(SensorController.class).one(sensor.getSid())).withSelfRel(),
+                linkTo(methodOn(SensorController.class).all()).withRel("sensors"));
+    }
+}
+

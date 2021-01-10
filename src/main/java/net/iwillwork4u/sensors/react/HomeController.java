8193b76279019582be57ec167c73d5f5249d1314
Dusty Saker <dustysaker@gmail.com>
2021-01-10 08:19:01 -0800

    first react commit

---
+++ src/main/java/net/iwillwork4u/sensors/react/HomeController.java
@@ -0,0 +1,12 @@
+package net.iwillwork4u.sensors.react;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
+}
